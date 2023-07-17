a2ec4c030cac2eda78095aebb904df3cc79a076a
Alexis <alexis.algoud@perrenot.eu>
2023-07-17 09:53:37 +0200

    wip sendmail

---
+++ service/MailService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dolibarr\Cowork;
+
+dol_include_once('/cowork/service/CoreService.php');
+
+use Dolibarr\Core\CoreService;
+
+class MailService extends CoreService {
+
+	/**
+	 * @param string $subject
+	 * @param string $from
+	 * @param string $to
+	 * @param string $message
+	 * @param array<MailFile> $files
+	 * @return bool|string
+	 */
+	public function sendMail(string $subject, string $message, string $from, string $to, array $files = []): bool
+	{
+
+		$filepath = $mimetype = $filename = [];
+		foreach($files as $file) {
+			$filepath = $file->path;
+			$mimetype = $file->mimetype;
+			$filename = $file->name;
+		}
+
+
+		$mailFile = new \CMailFile($subject, $to, $from, $message, $filepath, $mimetype, $filename);
+
+		if (false === $mailFile->sendfile()) {
+			throw new \Exception('Unable to send mail ['.$subject.'] to '.$to.' from '.$from);
+		}
+
+		return true;
+	}
+}
